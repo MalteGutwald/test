c64c2101209e6acccde72e4ca97a43482994fa76
Malte <mgutwald@techfak.de>
2021-10-04 14:01:21 +0200

    version 1

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>Test</h1>
+    <Inputs></Inputs>
+    <!--
+    <form>
+      <input v-on:click="greet" type="submit" value="greet" class="btn">
+    </form>
+    -->  
+  </div>
+
+
+</template>
+
+<script>
+import Inputs from '../components/Inputs.vue';
+
+export default {
+  components:{
+    Inputs
+  },
+
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
